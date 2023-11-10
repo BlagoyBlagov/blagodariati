aa5791968bf0afc3bf9435f3fdec82cf54b46a1f
Blagoy Blagov <blagoicho0@gmail.com>
2023-11-10 10:55:29 +0200

    Add footer

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -1,5 +1,15 @@
 const Footer = () => {
-  
-}
+
+    const date = new Date();
+    const year = date.getFullYear();
+
+    return (
+        <footer className="py-3 my-4">
+            <p className="text-center text-body-secondary">
+                &copy; {year} Благодаря Ти
+            </p>
+        </footer>
+    );
+};
 
 export default Footer;
